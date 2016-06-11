1d9a03001276a1fa0280f7340b19c1ac87528703
andrehNSFW <andresan2006@gmail.com>
2016-06-11 15:29:13 -0300

    Add bars for different buffer channels

--- js/webaudio.js
+++ js/webaudio.js
@@ -1,31 +1,30 @@
-function webAudio(url) {
+function audioBars(url) {
   this.url = url;  
 
   this.play = document.getElementById("play"); //Play button
-  this.meter = document.querySelectorAll('.meter'); //Get all audio meters
-  this.initialized = false;
 
+  this.meterWrapper = document.querySelector('.meter__wrapper'); //Meter wrapper
+  this.meterNumber = 30; //Number of bars
+  this.appendMeters();
+
+  this.initialized = false;
   this.bind();
 }
 
-webAudio.prototype = {
+audioBars.prototype = {
   //Setups the sound of the url passed as parameter of the class
   setup: function() {
-    var _this = this;
-        
-    this.channels = this.meter.length; //Set number of channels according the number of audio meters
+    var _this = this;          
 
     this.ctx = new AudioContext();
     this.audio = new Audio(this.url);
-    this.processor = this.ctx.createScriptProcessor(2048, this.channels, this.channels); // 2048 sample buffer, 1 channel in, 1 channel out  
+    this.processor = this.ctx.createScriptProcessor(2048, 1, 1); // 2048 sample buffer, 1 channel in, 1 channel out  
 
     this.source = this.ctx.createMediaElementSource(this.audio);
     this.source.connect(this.processor);
     this.source.connect(this.ctx.destination);
     this.processor.connect(this.ctx.destination);
-
-    // loop through PCM data and calculate average
-    // volume for a given 2048 sample buffer
+    
     this.processor.onaudioprocess = function(evt){
       _this.calcVolume(evt);
     }
@@ -49,20 +48,32 @@ webAudio.prototype = {
     });    
   },
 
+  //Append bars to meter wrapper
+  appendMeters() {
+    var wrapperSize = this.meterWrapper.offsetWidth;
+        meterWidth = wrapperSize / this.meterNumber;
+
+    for(var y = 0; y < this.meterNumber; y++) {
+      this.meterWrapper.innerHTML += "<div class='meter'>";
+    }
+
+    this.meter = document.querySelectorAll('.meter'); //Get all audio meters
+
+    for(var x = 0; x < this.meter.length; x++) {            
+      this.meter[x].style.width = meterWidth+"px";
+    }
+  },
+
   //Calculates loudness and sets meter width according to it
   calcVolume: function(evt) {
     var _this = this;        
+        input = evt.inputBuffer.getChannelData(0),
+        len = input.length;        
 
-    for(var x = 0; x < this.channels; x++) {
-      input = evt.inputBuffer.getChannelData(x),
-      len = input.length,
-      total = i = 0,
-      rms = 0;
+    console.log(len)
 
-      while ( i < len ) total += Math.abs( input[i++] );
-      rms = Math.sqrt( total / len );
-      
-      _this.meter[x].style.width = ( rms * 100 ) + '%';                  
-    }
+    for(var x = 0; x < _this.meter.length; x++) {            
+      _this.meter[x].style.height = ( Math.abs(input[x]) * 100 ) + '%';                                                    
+    }      
   }
 }
